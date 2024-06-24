dc3027558111d83a1599a1a1744e4f23bbab07e6
pavlir <irina.boltenkova@onevizion.com>
2024-06-24 12:14:28 +0700

    Format a Portfolio Component

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,31 +4,31 @@ import {ProjectList} from '../../components/ProjectList/ProjectList'
 import { useState } from 'react'
 
 export const Portfolio = (props) => {
-	const {projects, filters} = props;
-	const [selectedFilter, setSelectedFilter] = useState("All");
+  const {projects, filters} = props;
+  const [selectedFilter, setSelectedFilter] = useState("All");
 	
-	const handleFilterSelect = (filter) => {
-		setSelectedFilter(filter);
-	};
+  const handleFilterSelect = (filter) => {
+    setSelectedFilter(filter);
+  };
 
-	const getFilteredProjects = () => {
-		if (selectedFilter == "All") {
-			return projects;
-		}
+  const getFilteredProjects = () => {
+    if (selectedFilter == "All") {
+      return projects;
+    }
 		
     return projects.filter(project => project.category === selectedFilter);
   };
 
   const filteredProjects = getFilteredProjects();
 
-	return (
-		<div className="portfolio">
-			<div className="toolbar-container">
-				<Toolbar filters={filters} selected={selectedFilter} onSelectFilter={handleFilterSelect} />
-			</div>
-			<div className="project-list-container">
-				<ProjectList projects={filteredProjects}/>
-			</div>
-	</div>
-	)
+  return (
+    <div className="portfolio">
+      <div className="toolbar-container">
+        <Toolbar filters={filters} selected={selectedFilter} onSelectFilter={handleFilterSelect} />
+      </div>
+      <div className="project-list-container">
+        <ProjectList projects={filteredProjects}/>
+      </div>
+  </div>
+  )
 }
--- src/components/Toolbar/Toolbar.jsx
+++ src/components/Toolbar/Toolbar.jsx
@@ -5,11 +5,11 @@ export const Toolbar = (props) => {
   const { filters, selected, onSelectFilter } = props;
   const cFilters = filters.map(filter => {
     return <Button 
-        key={filter} 
-        btnName={filter}
-        className={filter == selected ? 'btn_filter selected' : 'btn_filter'} 
-        click={() => { onSelectFilter(filter); }
-      }/>
+      key={filter} 
+      btnName={filter}
+      className={filter == selected ? 'btn_filter selected' : 'btn_filter'} 
+      click={() => { onSelectFilter(filter); }
+    }/>
   });
 
   return (
